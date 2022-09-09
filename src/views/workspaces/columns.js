b2af9f012790bb244a6f16e8762deb07a8e57a3a
Ronak Patel <ronu5879@gmail.com>
2022-09-09 22:20:15 +0530

    user delete/cancel invite and nickname

--- src/views/workspaces/columns.js
+++ src/views/workspaces/columns.js
@@ -2,7 +2,7 @@
 import { Link } from "react-router-dom";
 
 // ** Custom Components
-// import Avatar from "@components/avatar";
+import Avatar from "@components/avatar";
 
 // ** Store & Actions
 // import { store } from "@store/store";
@@ -30,13 +30,30 @@ import {
   DropdownItem,
 } from "reactstrap";
 
-const renderName = (row) => row.name;
+const renderName = (row) => {
+  return row.nickname ? row.nickname : row.name;
+};
 const renderCreatedAt = (row) => {
   return row.createdAt ? row.createdAt : "-";
 };
 
 const renderJoinedAt = (row) => {
   return row.joinedAt ? row.joinedAt : "-";
+};
+
+const renderClient = (row) => {
+  if (row.avatar && row.avatar.length) {
+    return <Avatar className="me-1" img={row.avatar} width="32" height="32" />;
+  } else {
+    return (
+      <Avatar
+        initials
+        className="me-1"
+        color={row.avatarColor || "light-primary"}
+        content={row.name || "John Doe"}
+      />
+    );
+  }
 };
 
 export const adminWorkspaceColumns = [
@@ -176,11 +193,39 @@ export const userColumns = [
   {
     name: "Name",
     sortable: true,
-    minWidth: "172px",
+    minWidth: "300px",
     sortField: "name",
-    selector: (row) => row.name,
-    cell: (row) => renderName(row),
+    selector: (row) => {
+      return row.nickname ? row.nickname : row.name;
+    },
+    cell: (row) => (
+      <div className="d-flex justify-content-left align-items-center">
+        {renderClient(row)}
+        <div className="d-flex flex-column">
+          {/* <Link
+            to={`/apps/user/view/${row.id}`}
+            className='user_name text-truncate text-body'
+            onClick={() => store.dispatch(getUser(row.id))}
+          > */}
+          <span className="fw-bolder">
+            {row.nickname ? row.nickname : row.name}
+          </span>
+          {/* </Link> */}
+          <small className="text-truncate text-muted mb-0">{row.email}</small>
+        </div>
+      </div>
+    ),
   },
+  // {
+  //   name: "Name",
+  //   sortable: true,
+  //   minWidth: "172px",
+  //   sortField: "name",
+  //   selector: (row) => {
+  //     return row.nickname ? row.nickname : row.name;
+  //   },
+  //   cell: (row) => renderName(row),
+  // },
   {
     name: "Joined At",
     sortable: true,
@@ -189,52 +234,55 @@ export const userColumns = [
     selector: (row) => row.joinedAt,
     cell: (row) => renderJoinedAt(row),
   },
-  // {
-  //   name: "Actions",
-  //   minWidth: "100px",
-  //   cell: (row) => (
-  //     <div className="column-action">
-  //       <UncontrolledDropdown>
-  //         <DropdownToggle tag="div" className="btn btn-sm">
-  //           <MoreVertical size={14} className="cursor-pointer" />
-  //         </DropdownToggle>
-  //         <DropdownMenu>
-  //           <DropdownItem
-  //             tag={Link}
-  //             className="w-100"
-  //             to={`/workspace/${row.id}`}
-  //             // onClick={() => store.dispatch(getUser(row.id))}
-  //           >
-  //             <FileText size={14} className="me-50" />
-  //             <span className="align-middle">Details</span>
-  //           </DropdownItem>
-  //           <DropdownItem
-  //             tag="a"
-  //             href="/"
-  //             className="w-100"
-  //             onClick={(e) => {
-  //               row.handleEdit(row.id);
-  //               e.preventDefault();
-  //             }}
-  //           >
-  //             <Archive size={14} className="me-50" />
-  //             <span className="align-middle">Edit</span>
-  //           </DropdownItem>
-  //           <DropdownItem
-  //             tag="a"
-  //             href="/"
-  //             className="w-100"
-  //             onClick={(e) => {
-  //               row.handleDelete(row.id);
-  //               e.preventDefault();
-  //             }}
-  //           >
-  //             <Trash2 size={14} className="me-50" />
-  //             <span className="align-middle">Delete</span>
-  //           </DropdownItem>
-  //         </DropdownMenu>
-  //       </UncontrolledDropdown>
-  //     </div>
-  //   ),
-  // },
+  {
+    name: "Actions",
+    minWidth: "100px",
+    cell: (row) => (
+      <div className="column-action">
+        <UncontrolledDropdown>
+          <DropdownToggle tag="div" className="btn btn-sm">
+            <MoreVertical size={14} className="cursor-pointer" />
+          </DropdownToggle>
+          <DropdownMenu>
+            {/* <DropdownItem
+              tag={Link}
+              className="w-100"
+              to={`/workspace/${row.id}`}
+              // onClick={() => store.dispatch(getUser(row.id))}
+            >
+              <FileText size={14} className="me-50" />
+              <span className="align-middle">Details</span>
+            </DropdownItem> */}
+            <DropdownItem
+              tag="a"
+              href="/"
+              className="w-100"
+              onClick={(e) => {
+                row.handleEdit(row.id);
+                e.preventDefault();
+              }}
+            >
+              <Archive size={14} className="me-50" />
+              <span className="align-middle">Edit</span>
+            </DropdownItem>
+
+            <DropdownItem
+              tag="a"
+              href="/"
+              className="w-100"
+              onClick={(e) => {
+                row.handleDelete(row.invitationId, row.joinedAt);
+                e.preventDefault();
+              }}
+            >
+              <Trash2 size={14} className="me-50" />
+              <span className="align-middle">
+                {row.joinedAt ? "Remove user" : "Cancel invitation"}
+              </span>
+            </DropdownItem>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      </div>
+    ),
+  },
 ];
