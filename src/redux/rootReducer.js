9afb387b53113c6814890d151b7b78978f88a219
ikramkhizer <ikramkhizer@gmail.com>
2022-10-21 16:29:29 +0500

    notification module

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -5,7 +5,8 @@ import auth from "./auth";
 import workspaces from "./workspaces";
 import plans from "./plans";
 import feedbacks from "./feedbacks";
+import notifications from "./notifications";
 
-const rootReducer = { navbar, layout, auth, workspaces, plans, feedbacks };
+const rootReducer = { navbar, layout, auth, workspaces, plans, feedbacks, notifications };
 
 export default rootReducer;
