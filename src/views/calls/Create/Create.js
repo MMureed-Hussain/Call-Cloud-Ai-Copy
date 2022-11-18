70e72f14b727e08d2e1daf22cafbeb805e5c1f95
Umair Hassan <umairhassan.upwork@gmail.com>
2022-11-18 16:15:14 +0500

    changes

--- src/views/calls/Create/Create.js
+++ src/views/calls/Create/Create.js
@@ -1,75 +1,93 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 import {
-    Card,
-    Form,
-    Input,
-    Label,
-    Col,
-    FormGroup,
-    CardBody,
-    Button
+  Card,
+  Form,
+  Input,
+  Label,
+  Col,
+  Row,
+  FormGroup,
+  CardBody,
+  Button,
 } from "reactstrap";
 import Recorder from "./components/Recorder";
 
 export default () => {
-    const [audioDetails, setAudioDetails] = useState({
-        url: null,
-        blob: null,
-        chunks: [],
-        duration: {
-            h: 0,
-            m: 0,
-            s: 0
-        }
-    });
-    const [phone, setPhone] = useState("");
-    const [note, setNote] = useState("");
-    //   const [tags, setTags] = useState("");
+  const [audioDetails, setAudioDetails] = useState({
+    url: null,
+    blob: null,
+    chunks: [],
+    duration: {
+      h: 0,
+      m: 0,
+      s: 0,
+    },
+  });
+  const [phone, setPhone] = useState("");
+  const [note, setNote] = useState("");
+  //   const [tags, setTags] = useState("");
 
-    useEffect(() => {
-        return () => {
-            //todo remove event listeners
-        }
-    }, [audioDetails])
+  useEffect(() => {
+    return () => {
+      //todo remove event listeners
+    };
+  }, [audioDetails]);
 
-    return (
-        <Card>
-            <Col md="8" sm="12">
-                <Form>
-                    <CardBody>
-                        <FormGroup>
-                            <Label className="form-label" for="title">
-                                Phone Number<span className="text-danger">*</span>
-                            </Label>
-                            <Input
-                                id="phone"
-                                placeholder="Enter phone number"
-                                value={phone}
-                                onChange={(e) => {
-                                    setPhone(e.target.value);
-                                }}
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label className="form-label" for="title">
-                                Note<span className="text-danger">*</span>
-                            </Label>
-                            <Input
-                                id="note"
-                                placeholder="Enter note"
-                                value={note}
-                                onChange={(e) => setNote(e.target.value)}
-                            />
-                        </FormGroup>
-                        <FormGroup>
+  return (
+    <Card>
+      <Row>
+        <Col md="6" sm="6">
+          <Form>
+            <CardBody>
+              <FormGroup>
+                <Label className="form-label" for="title">
+                  Phone Number<span className="text-danger">*</span>
+                </Label>
+                <Input
+                  id="phone"
+                  placeholder="Enter phone number"
+                  value={phone}
+                  onChange={(e) => {
+                    setPhone(e.target.value);
+                  }}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label className="form-label" for="title">
+                  Note<span className="text-danger">*</span>
+                </Label>
+                <Input
+                  id="note"
+                  placeholder="Enter note"
+                  value={note}
+                  onChange={(e) => setNote(e.target.value)}
+                />
+              </FormGroup>
+              {/* <FormGroup>
                             <Label className="form-label" for="title">
                                 Voice<span className="text-danger">*</span>
                             </Label>
                             <Recorder audioDetails={audioDetails} setAudioDetails={setAudioDetails} />
-                        </FormGroup>
-                    </CardBody>
-                </Form>
-            </Col>
-        </Card>
-    );
+                        </FormGroup> */}
+            </CardBody>
+          </Form>
+        </Col>
+        <Col md="6" sm="6">
+          <Form>
+            <CardBody>
+              <FormGroup>
+                <Label className="form-label" for="title">
+                  Voice<span className="text-danger">*</span>
+                </Label>
+                <Recorder
+                  audioDetails={audioDetails}
+                  setAudioDetails={setAudioDetails}
+                />
+              </FormGroup>
+            </CardBody>
+          </Form>
+        </Col>
+      </Row>
+    </Card>
+  );
 };
