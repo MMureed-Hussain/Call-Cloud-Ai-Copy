ca582dcaee5091f3e99ed54504ec4147033c8e4e
Umair Hassan <umairhassan.upwork@gmail.com>
2022-12-07 20:15:51 +0500

    delete_pipeline

--- src/redux/pipelines.js
+++ src/redux/pipelines.js
@@ -3,107 +3,120 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 // ** Axios Imports
 import axiosPkg from "axios";
 import toast from "react-hot-toast";
-import ErrorHandler from "../utility/ErrorHandler"
+import ErrorHandler from "../utility/ErrorHandler";
 
 const axios = axiosPkg.create({
-    baseURL: `${process.env.REACT_APP_API_ENDPOINT}/api/pipelines`,
-    withCredentials: true
-})
+  baseURL: `${process.env.REACT_APP_API_ENDPOINT}/api/pipelines`,
+  withCredentials: true,
+});
 
 export const pipelinesSlice = createSlice({
-    name: "pipelines",
-    initialState: {
-        errors: new ErrorHandler(),
-        pipelines: []
+  name: "pipelines",
+  initialState: {
+    errors: new ErrorHandler(),
+    pipelines: [],
+    reloadTable: false
+  },
+  reducers: {
+    setErrors: (state, { payload }) => {
+      state.errors.setErrors(payload);
     },
-    reducers: {
-        setErrors: (state, { payload }) => {
-            state.errors.setErrors(payload);
-        },
-        setPipelines: (state, { payload }) => {
-            state.pipelines = payload
-        },
-        setNewPipeline: (state, { payload }) => {
-            const pipelines = [...state.pipelines, payload];
-            state.pipelines = pipelines
-        },
-        setUpdatedPipeline: (state, { payload }) => {
-            let pipelines = [...state.pipelines];
-            let index = pipelines.findIndex(p => p.id == payload.id);
-            pipelines[index].name = payload.name;
-            state.pipelines = pipelines
-        },
+    setPipelines: (state, { payload }) => {
+      state.pipelines = payload;
+    },
+    setNewPipeline: (state, { payload }) => {
+      const pipelines = [...state.pipelines, payload];
+      state.pipelines = pipelines;
+    },
+    setUpdatedPipeline: (state, { payload }) => {
+      let pipelines = [...state.pipelines];
+      let index = pipelines.findIndex((p) => p.id == payload.id);
+      pipelines[index].name = payload.name;
+      state.pipelines = pipelines;
+    },
+    setReloadTable: (state, { payload }) => {
+        state.reloadTable = payload;
     }
+  },
 });
 
 export const getPipelines = createAsyncThunk(
-    "pipelines/index",
-    async (workspaceId, { dispatch }) => {
-        try {
-            const response = await axios.get(
-                `/`,
-                {
-                    params: { workspace_id: workspaceId }
-                }
-            );
-            dispatch(setPipelines(response.data.data))
-        } catch (e) {
-            toast.error(e.response.data.message);
-        }
+  "pipelines/index",
+  async (workspaceId, { dispatch }) => {
+    try {
+      const response = await axios.get(`/`, {
+        params: { workspace_id: workspaceId },
+      });
+      dispatch(setPipelines(response.data.data));
+    } catch (e) {
+      toast.error(e.response.data.message);
     }
+  }
 );
 
 export const createPipeline = createAsyncThunk(
-    "pipelines/create",
-    async (payload, { dispatch }) => {
-        try {
-            const response = await axios.post(
-                `/`,
-                payload,
-            );
-            toast.success(response.data.message);
-            dispatch(setNewPipeline(response.data.data))
-            return {
-                data: true
-            };
-        } catch (e) {
-            toast.error(e.response.data.message);
-            if (e.response.data?.errors) dispatch(setErrors(e.response.data.errors));
-            return {
-                data: false
-            };
-        }
+  "pipelines/create",
+  async (payload, { dispatch }) => {
+    try {
+      const response = await axios.post(`/`, payload);
+      toast.success(response.data.message);
+      dispatch(setNewPipeline(response.data.data));
+      return {
+        data: true,
+      };
+    } catch (e) {
+      toast.error(e.response.data.message);
+      if (e.response.data?.errors) dispatch(setErrors(e.response.data.errors));
+      return {
+        data: false,
+      };
     }
+  }
 );
 
 export const updatePipeline = createAsyncThunk(
-    "pipelines/update",
-    async ({ id, formData }, { dispatch }) => {
-        try {
-            const response = await axios.put(
-                `/${id}`,
-                formData,
-            );
-            toast.success(response.data.message);
-            dispatch(setUpdatedPipeline({ id, name: formData.name }));
-            return {
-                data: true
-            };
-        } catch (e) {
-            toast.error(e.response.data.message);
-            if (e.response.data?.errors) dispatch(setErrors(e.response.data.errors));
-            return {
-                data: false
-            };
-        }
+  "pipelines/update",
+  async ({ id, formData }, { dispatch }) => {
+    try {
+      const response = await axios.put(`/${id}`, formData);
+      toast.success(response.data.message);
+      dispatch(setUpdatedPipeline({ id, name: formData.name }));
+      return {
+        data: true,
+      };
+    } catch (e) {
+      toast.error(e.response.data.message);
+      if (e.response.data?.errors) dispatch(setErrors(e.response.data.errors));
+      return {
+        data: false,
+      };
     }
+  }
 );
 
-export const {
-    setErrors,
-    setPipelines,
-    setNewPipeline,
-    setUpdatedPipeline
-} = pipelinesSlice.actions;
+// Delete
+export const deletePipeline = createAsyncThunk(
+  "pipelines/delete",
+  async (url, { dispatch }) => {
+    try {
+      const response = await axios.delete(url);
+      toast.success(response.data.message);
+      dispatch(setReloadTable(true));
+    //   window.location.reload(false);
+      return {
+        data: true,
+      };
+    } catch (e) {
+      toast.error(e.response.data.message);
+      return {
+        data: false,
+      };
+    }
+  }
+);
+
+export const { setErrors, setPipelines, setNewPipeline, setUpdatedPipeline, setReloadTable } =
+  pipelinesSlice.actions;
 
 export default pipelinesSlice.reducer;
+
--- src/views/pipelines/Index.js
+++ src/views/pipelines/Index.js
@@ -1,110 +1,148 @@
 // ** React Imports
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 // ** Third Party Components
-import { ReactSortable } from 'react-sortablejs'
+import { ReactSortable } from "react-sortablejs";
 
 // ** Reactstrap Imports
-import { Card, CardHeader, CardTitle, CardBody, CardText, ListGroupItem, Button, UncontrolledDropdown, DropdownItem, DropdownMenu, DropdownToggle } from 'reactstrap'
-import { MoreVertical, Edit, Trash } from "react-feather"
-import PipelineSidebar from './components/PipelineSidebar'
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardBody,
+  CardText,
+  ListGroupItem,
+  Button,
+  UncontrolledDropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+} from "reactstrap";
+import { MoreVertical, Edit, Trash } from "react-feather";
+import PipelineSidebar from "./components/PipelineSidebar";
 import { useDispatch, useSelector } from "react-redux";
-import { getPipelines, setPipelines } from "../../redux/pipelines";
+import {
+  getPipelines,
+  setPipelines,
+  deletePipeline,
+} from "../../redux/pipelines";
 import Skeleton from "react-loading-skeleton";
 
 export default () => {
-    // ** State
-    const [sidebarOpen, setSidebarOpen] = useState(false);
-    const [selectedPipeline, setSelectedPipeline] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
-    const pipelines = useSelector(state => state.pipelines.pipelines);
-    const currentWorkspace = useSelector(state => state.workspaces.currentWorkspace);
+  // ** State
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [selectedPipeline, setSelectedPipeline] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const pipelines = useSelector((state) => state.pipelines.pipelines);
+  const currentWorkspace = useSelector(
+    (state) => state.workspaces.currentWorkspace
+  );
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        if (currentWorkspace) {
-            setIsLoading(true);
-            dispatch(getPipelines(currentWorkspace.id))
-        }
-    }, [currentWorkspace])
-
-    useEffect(() => {
-        setIsLoading(false);
-    }, [pipelines])
-
-    const onUpdate = () => {
-        console.log("onUpdate");
+  useEffect(() => {
+    if (currentWorkspace) {
+      setIsLoading(true);
+      dispatch(getPipelines(currentWorkspace.id));
     }
+  }, [currentWorkspace]);
 
-    const toggleSidebar = () => {
-        setSidebarOpen(!sidebarOpen);
-    };
-    
-    if (isLoading) {
-        return (
-            <div className="vh-100">
-                <Skeleton height={"15%"} />
-                <Skeleton height={"7%"} count={9} />
-            </div>
-        );
-    }
+  useEffect(() => {
+    setIsLoading(false);
+  }, [pipelines]);
 
+  const onUpdate = () => {
+    console.log("onUpdate");
+  };
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
+  if (isLoading) {
     return (
-        <>
-            <Card>
-                <CardHeader>
-                    <CardTitle tag='h4'>Pipelines</CardTitle>
-                    <div className="d-flex align-items-center table-header-actions">
-                        <Button className="add-new-user" color="primary" onClick={toggleSidebar}>
-                            Add Pipeline
-                        </Button>
-                    </div>
-                </CardHeader>
-                <CardBody>
-                    <CardText>
-                        Click on the row and sort up or down to sort pipelines.
-                    </CardText>
-                    <ReactSortable onUpdate={onUpdate} tag='ul' animation={300} className='list-group' list={pipelines} setList={(data) => dispatch(setPipelines(data))} delayOnTouchStart={true} delay={2}>
-                        {pipelines.map(item => {
-                            return (
-                                <ListGroupItem className='draggable' key={item.name}>
-                                    <div className='d-flex justify-content-between'>
-                                        <h5 className='mt-0'>{item.name}</h5>
-                                        <div className="d-flex">
-                                            <UncontrolledDropdown>
-                                                <DropdownToggle className="pe-1" tag="span">
-                                                    <MoreVertical size={15} />
-                                                </DropdownToggle>
-                                                <DropdownMenu end>
-                                                    <DropdownItem onClick={() => {
-                                                        setSelectedPipeline(item);
-                                                        toggleSidebar();
-                                                    }}>
-                                                        <Edit size={15} />
-                                                        <span className="align-middle ms-50">Edit</span>
-                                                    </DropdownItem>
-                                                    <DropdownItem onClick={() => console.log("onclick")}>
-                                                        <Trash size={15} className="me-50" />
-                                                        <span
-                                                            className="align-middle ms-50"
-                                                        >
-                                                            Delete
-                                                        </span>
-                                                    </DropdownItem>
-                                                </DropdownMenu>
-                                            </UncontrolledDropdown>
-                                        </div>
-                                    </div>
-                                </ListGroupItem>
-                            )
-                        })}
-                    </ReactSortable>
-                </CardBody>
-            </Card>
-            {sidebarOpen && (
-                <PipelineSidebar open={sidebarOpen} toggleSidebar={toggleSidebar} pipeline={selectedPipeline} />
-            )}
-        </>
+      <div className="vh-100">
+        <Skeleton height={"15%"} />
+        <Skeleton height={"7%"} count={9} />
+      </div>
+    );
+  }
 
-    )
-}
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle tag="h4">Pipelines</CardTitle>
+          <div className="d-flex align-items-center table-header-actions">
+            <Button
+              className="add-new-user"
+              color="primary"
+              onClick={toggleSidebar}
+            >
+              Add Pipeline
+            </Button>
+          </div>
+        </CardHeader>
+        <CardBody>
+          <CardText>
+            Click on the row and sort up or down to sort pipelines.
+          </CardText>
+          <ReactSortable
+            onUpdate={onUpdate}
+            tag="ul"
+            animation={300}
+            className="list-group"
+            list={pipelines}
+            setList={(data) => dispatch(setPipelines(data))}
+            delayOnTouchStart={true}
+            delay={2}
+          >
+            {pipelines.map((item) => {
+              return (
+                <ListGroupItem className="draggable" key={item.name}>
+                  <div className="d-flex justify-content-between">
+                    <h5 className="mt-0">{item.name}</h5>
+                    <div className="d-flex">
+                      <UncontrolledDropdown>
+                        <DropdownToggle className="pe-1" tag="span">
+                          <MoreVertical size={15} />
+                        </DropdownToggle>
+                        <DropdownMenu end>
+                          <DropdownItem
+                            onClick={() => {
+                              setSelectedPipeline(item);
+                              toggleSidebar();
+                            }}
+                          >
+                            <Edit size={15} />
+                            <span className="align-middle ms-50">Edit</span>
+                          </DropdownItem>
+
+                          <DropdownItem
+                            onClick={() => dispatch(deletePipeline(`${process.env.REACT_APP_API_ENDPOINT}/api/pipelines/${item.id}`
+                                )
+                              )
+                            }
+                          >
+                            <Trash size={15} className="me-50" />
+                            <span className="align-middle ms-50">Delete</span>
+                          </DropdownItem>
+                        </DropdownMenu>
+                      </UncontrolledDropdown>
+                    </div>
+                  </div>
+                </ListGroupItem>
+              );
+            })}
+          </ReactSortable>
+        </CardBody>
+      </Card>
+      {sidebarOpen && (
+        <PipelineSidebar
+          open={sidebarOpen}
+          toggleSidebar={toggleSidebar}
+          pipeline={selectedPipeline}
+        />
+      )}
+    </>
+  );
+};
